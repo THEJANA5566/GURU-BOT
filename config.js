aabd43b597e9cb565d7362de273cfb4222f17e4b
THEJANA5566 <158037524+THEJANA5566@users.noreply.github.com>
2024-01-29 03:35:10 -0800

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94706345938', 'me', true],
+  ['94706345938', 'NANDUN', true], 
   [''] 
 ] //Number of owners
 
